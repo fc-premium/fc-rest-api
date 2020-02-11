32e5ae6d702f2e6c4fcd6117b48406f7c8f32384
pytness <pytness@gmail.com>
2020-02-11 13:26:34 +0100

    Removed js/

--- js/user/index.js
+++
@@ -1,5 +0,0 @@
-export * from './user-abouts';
-export * from './user-stats';
-export * from './basic-user';
-export * from './user';
-export * from './current-user';
--- js/user/user-abouts.js
+++
@@ -1,9 +0,0 @@
-export class UserAbouts {
-    constructor() {
-        this.car = null;
-        this.place = null;
-        this.interests = null;
-        this.occupation = null;
-        this.signature = null;
-    }
-}
